a1d4a5fa46d171f4efe81005dc6f1817d348cc59
crazle <crazle@mail.ru>
2023-11-27 11:54:39 +0300

    Revert "BOW redacted again"
    This reverts commit 0c92454ac94ff61972faa40d8eb5beb113752d87.

--- HotDrink.java
+++ HotDrink.java
@@ -4,14 +4,6 @@ public class HotDrink extends BottleOfWater{
 		super(name, cost, volume);
 		this.temperature = temperature;
 	}
-	public int getTemperature(){
-		return temperature;
-	}
-	public void setTemperature(int temperature){
-		this.temperature = temperature;
-	}
-	@Override
-	public String toString(){
-		return "BottleOfWater{" + "name=" + super.getName() + ";cost=" + super.getCost + ";volume=" + super.volume + ";temperature=" temperature"}";
+
 	}
 }
