b3c5e6f2c78b35009670f856188e6c4593f19e3f
crazle <crazle@mail.ru>
2023-11-27 12:07:50 +0300

    Revert "BottleOfWater redacted in semin2" by Task2
    This reverts commit dd1ed77bf9c0848ede6eb256e9cab3181bfae5ab.

--- BottleOfWater.java
+++ BottleOfWater.java
@@ -4,5 +4,14 @@ public class BottleOfWater extends Product{
 		super(name, cost);
 		this.volume = volue;
 	}
-	
+	public int getVolume(){
+		return volume;
+	}
+	public void setVolume(int volume){
+		this.volume = volume;
+	}
+	@Override
+	public String toString(){
+		return "BottleOfWater{" + "name=" + super.getName() + ";cost=" + super.getCost + ";volume=" + volume + "}";
+	}
 }
